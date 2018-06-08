6076522cda0fe0ebdc317f65835d3677b308dd2e
Jose Mena <josemena0114@gmail.com>
2018-06-08 13:35:19 -0400

    added react router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
+import * as firebase from 'firebase';
 import logo from './logo.svg';
 import './App.css';
+
+
+import RoomList from './components/RoomList';
+
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDCqoibibgw-BzBSurWmYJuRpgXWUByeLY",
+    authDomain: "bloc-chat-rooms-mena.firebaseapp.com",
+    databaseURL: "https://bloc-chat-rooms-mena.firebaseio.com",
+    projectId: "bloc-chat-rooms-mena",
+    storageBucket: "bloc-chat-rooms-mena.appspot.com",
+    messagingSenderId: "391599309588"
+  };
+  firebase.initializeApp(config);
+
 
 class App extends Component {
   render() {
