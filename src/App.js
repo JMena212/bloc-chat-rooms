745c7b055cab8d912daa281630f8459a7939de28
Jose Mena <josemena0114@gmail.com>
2018-06-11 20:50:20 -0400

    mapping rooms and adding RoomList nav to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Route, Link } from 'react-router-dom';
 import * as firebase from 'firebase';
 import logo from './logo.svg';
 import './App.css';
-
-
 import RoomList from './components/RoomList';
 
 // Initialize Firebase
@@ -22,13 +22,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <nav className="navbar">
+            <div className = "links-container">
+            <Link to='/RoomList' className="navbar-link">Chat Rooms</Link>
+
+       </div>
+            </nav>
+            <Route exact path="/" component={App} />
+            <Route path="/roomlist" component={RoomList} />
       </div>
     );
   }
