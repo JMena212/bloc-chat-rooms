619753a37d0a32762834042678f60f4377d2740c
Jose Mena <josemena0114@gmail.com>
2018-06-14 17:03:26 -0700

    room create form rendering

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,49 +1,50 @@
-import React, { Component } from 'react';
-import * as firebase from 'firebase';
+import React, { Component } from "react";
+import * as firebase from "firebase";
 
 class RoomList extends Component {
   constructor(props) {
-      super(props);
-      this.state = {rooms: []};
-      this.roomsRef = this.props.firebase.database().ref('rooms');
+    super(props);
+    this.state = { rooms: [] };
+    this.roomsRef = this.props.firebase.database().ref("rooms");
   }
-
 
   handleRoomCreation(event) {
     event.preventDefault();
-    this.roomsRef.push({room_name: this.state.room_name})
+    this.roomsRef.push({ room_name: this.state.roomName });
   }
 
   componentDidMount() {
+    this.roomsRef.on("child_added", snapshot => {
+      var room = { roomName: snapshot.val(), key: snapshot.key };
+      this.setState({ rooms: this.state.rooms.concat(room) });
+    });
+  }
 
-      this.roomsRef.on('child_added', snapshot => {
-        var room = {roomName: snapshot.val(), key: snapshot.key}
-        this.setState({ rooms: this.state.rooms.concat( room ) });
-      }
-    }
-
-
-    render() {
-      return (
-        <div>
-          {this.state.rooms.map((room) => {}
-             <div key={room.key}>{room.room_name} </div>
-          ))}
-          <Form handleRoomCreation={this.handleRoomCreation} handleChange={this.handleChange} roomName={this.state.roomName} />
-        </div>
-    )}
-
+  render() {
+    console.error(this.state.rooms)
+    return (
+      <div>
+        <Rooms data={this.state.rooms} />
+        <Form
+          handleRoomCreation={this.handleRoomCreation}
+          handleChange={this.handleChange}
+          roomName={this.state.roomName}
+        />
+      </div>
+    );
+  }
 }
-
 
 export default RoomList;
 
-const Form = ({handleRoomCreation, handleChange, roomName}) => (
+const Rooms = ({data}) => data.map(room => <div key={room.key}>{room.roomName} </div>)
+
+const Form = ({ handleRoomCreation, handleChange, roomName }) => (
   <form onSubmit={handleRoomCreation}>
-   <label>
-     Room Name:
-     <input type="text" value={roomName} onChange={handleChange} />
-   </label>
-   <input type="submit" value="Submit" />
+    <label>
+      Room Name:
+      <input type="text" value={roomName} onChange={handleChange} />
+    </label>
+    <input type="submit" value="Submit" />
   </form>
-)
+);
