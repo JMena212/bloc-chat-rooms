cfdc142ebe2eae5f63e412108a3fea6c6ac2c415
Jose Mena <josemena0114@gmail.com>
2018-08-02 18:01:14 -0400

    log in process almost there

--- src/components/User.js
+++ src/components/User.js
@@ -6,28 +6,43 @@ import ".././styles/chat.css";
 class User extends Component {
   constructor(props) {
     super(props);
-    this.state = {user: ""};
-    this.userRef = new x.auth.GoogleAuthProvider()
+    this.state = { user: "" };
+    this.userRef = new x.auth.GoogleAuthProvider();
+    const provider = new x.auth.GoogleAuthProvider();
   }
 
   handleSignIn() {
     console.log(new x.auth.GoogleAuthProvider());
 
     const provider = new x.auth.GoogleAuthProvider();
-    firebase
-      .auth()
-      .signInWithRedirect(provider)
-      .then(function(result) {
-        var token = result.credential.accessToken;
-        var user = result.user;
-        this.props.setUser(user);
-      }).catch(function(error) {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        var email = error.email;
-        var credential = error.credential;
-      });
-
+    var user = firebase.auth().currentUser;
+    var name, email, photoUrl, uid, emailVerified;
+    if (user != null) {
+    user.providerData.forEach(function (profile) {
+    console.log("Sign-in provider: " + profile.providerId);
+    console.log("  Provider-specific UID: " + profile.uid);
+    console.log("  Name: " + profile.displayName);
+    console.log("  Email: " + profile.email);
+    console.log("  Photo URL: " + profile.photoURL);
+  });
+    } else {
+      firebase
+        .auth()
+        .signInWithRedirect(provider)
+        .then(function(result) {
+          var token = result.credential.accessToken;
+          var user = result.user;
+          this.props.setUser(user);
+        })
+        .catch(function(error) {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          var email = error.email;
+          var credential = error.credential;
+          console.log(emailVerified);
+        });
+    }
+    console.log(emailVerified);
   }
 
   handleSignOut() {
@@ -45,26 +60,22 @@ class User extends Component {
     });
   }
 
-  handleLogIn(){
-    var user = firebase.auth().currentUser;
-    if (user){
-      this.handleSignOut()
-    }else {
-      this.handleSignIn()
-    }
-  }
-
-
-
   render() {
     const signIn = "Sign In";
     const signOut = "Sign Out";
 
-
     return (
       <div>
         <span onClick={() => this.handleSignIn(signIn)}> Sign In </span>
-        <span onClick={() => this.handleSignOut(signOut)}> Sign Out </span>
+        <span
+          onClick={() => {
+            if (window.confirm("Are you sure you want to sign out?"))
+              this.handleSignOut(signOut);
+          }}
+        >
+          {" "}
+          Sign Out{" "}
+        </span>
       </div>
     );
   }
