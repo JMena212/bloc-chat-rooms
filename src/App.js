a6dad20e3d22daa850bfdc6f7017352960c00a55
Jose Mena <josemena0114@gmail.com>
2018-08-07 13:13:28 -0400

    username displayed correctly

--- src/App.js
+++ src/App.js
@@ -2,29 +2,76 @@ import React, { Component } from "react";
 import RoomList from "./components/RoomList";
 import MessageList from "./components/MessageList";
 import User from "./components/User";
+import firebase from "./lib/firebase.js";
+import x from "firebase";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {roomId: "", user: ""};
-  }
+    this.state = { roomId: "", user: null };
+    this.userRef = new x.auth.GoogleAuthProvider();
 
-  setUser(user){
-    this.setState({user: user})
-    console.log(user)
   }
 
   selectRoom(room) {
-    this.setState({roomId: room.key})
+    this.setState({ roomId: room.key });
+  }
 
+  setUser(user) {
+    this.setState({ user: user });
+    if (this.state.user !== null){
+      console.log(user.displayName);
+    }
+  }
+
+
+  handleSignIn() {
+    const provider = new x.auth.GoogleAuthProvider();
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(function(result) {
+        var token = result.credential.accessToken;
+        this.setUser(result.user);
+      })
+      .catch(function(error) {
+        console.log(error);
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;
+      });
+
+  }
+
+  handleSignOut() {
+    const provider = new x.auth.GoogleAuthProvider();
+    firebase
+      .auth()
+      .signOut()
+      .then(function() {})
+      .catch(function(error) {});
   }
 
 
   render() {
+    var currentUser = null;
+
+    if (this.state.user !== null){
+      currentUser = this.state.user.displayName
+    } else {
+      currentUser = "Guest"
+    }
+
+
     return (
       <div>
-        <User setUser={this.setUser.bind(this)}/>
-        <RoomList handleClick={this.selectRoom.bind(this)}/>
+        <User setUser={this.setUser.bind(this)}
+         signIn={this.handleSignIn.bind(this)}
+         signOut={this.handleSignOut.bind(this)}
+         displayUser={currentUser}
+        />
+        <RoomList handleClick={this.selectRoom.bind(this)} />
         <MessageList roomId={this.state.roomId} />
       </div>
     );
--- src/components/User.js
+++ src/components/User.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import firebase from "../lib/firebase.js";
 import x from "firebase";
 import ".././styles/chat.css";
 
@@ -8,66 +7,32 @@ class User extends Component {
     super(props);
     this.state = { user: "" };
     this.userRef = new x.auth.GoogleAuthProvider();
-    const provider = new x.auth.GoogleAuthProvider();
-  }
-
-  handleSignIn() {
-    /*console.log(new x.auth.GoogleAuthProvider());*/
-    const provider = new x.auth.GoogleAuthProvider();
-    var user = firebase.auth().currentUser;
-    console.log(firebase.auth().currentUser)
-    var name, email, photoUrl, uid, emailVerified;
-
-    firebase
-      .auth()
-      .signInWithPopup(provider)
-      .then(function(result) {
-        var token = result.credential.accessToken;
-        this.props.setUser(result.user);
-        window.localStorage.setItem("user", "user");
-      })
-      .catch(function(error) {
-        window.localStorage.setItem("error", error)
-        console.log(error);
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        var email = error.email;
-        var credential = error.credential;
-      });
-  }
-
-  handleSignOut() {
-    const provider = new x.auth.GoogleAuthProvider();
-    firebase
-      .auth()
-      .signOut()
-      .then(function() {})
-      .catch(function(error) {});
   }
 
   componentDidMount() {
-    firebase.auth().onAuthStateChanged(user => {
+    x.auth().onAuthStateChanged(user => {
       this.props.setUser(user);
     });
   }
 
   render() {
-    const signIn = "Sign In";
-    const signOut = "Sign Out";
-    window.localStorage.setItem("error", "error")
+
+
 
     return (
       <div>
-        <span onClick={() => this.handleSignIn(signIn)}> Sign In </span>
-        <span
-          onClick={() => {
-            if (window.confirm("Are you sure you want to sign out?"))
-              this.handleSignOut(signOut);
-          }}
-        >
-          {" "}
-          Sign Out{" "}
-        </span>
+      <button onClick={() => this.props.signIn()}> Sign In </button>
+
+      <button
+        onClick={() => {
+          if (window.confirm("Are you sure you want to sign out?"))
+            this.props.signOut();
+        }}
+      >
+        {" "}
+        Sign Out{" "}
+      </button>
+      <h2> Welcome, {this.props.displayUser}</h2>
       </div>
     );
   }
